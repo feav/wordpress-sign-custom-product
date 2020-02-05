3386f812fba0aedc5e1780526d9f0245e06d79ae
Armel E <armelem@ausiteodit.com>
2020-02-05 14:24:58 +0100

    Choisir une image sur laquelle on va simuler la gravure

--- wordpress-sign-custom-product.php
+++ wordpress-sign-custom-product.php
@@ -20,15 +20,18 @@ define('WPSCP_API_URL_SITE', get_site_url() . "/");
 
 
 class SignCustomProduct {
-	
+	private $meta_key_img;
+
     function __construct() {
-		
+		$this->meta_key_img = 'image_gravure';
 		add_action( 'woocommerce_before_add_to_cart_button', array($this, 'content_before_add_tocart_button') );
 		add_filter( 'woocommerce_add_cart_item_data', array($this, 'add_cart_item_data'), 25, 2 );
 		add_filter( 'woocommerce_get_item_data', array($this, 'get_item_data') , 25, 2 );
 		add_action( 'woocommerce_add_order_item_meta', array($this, 'add_order_item_meta') , 10, 2 );
 		
 		add_action('wp_head', array($this, 'loadFonts') );
+		add_action('admin_menu', array( &$this , 'register_metabox'));
+       	add_action('save_post',  array(&$this, 'misha_save'), 1, 2 );
 
     }   
 
@@ -37,9 +40,134 @@ class SignCustomProduct {
      */
     
     /*
-     * ADD ACTION
+     * ADD META
      */
-	 
+	 static function register_metabox(){
+        	add_meta_box('Gravure', 'Gravure Produit', array( &$this, 'product_gravure_img'), 'product', 'side', 'high');
+		}
+	/*
+	* CALL BACK META
+	*/
+	public function product_gravure_img(){
+		global $post;
+		$template = '';
+		 $name = get_post_meta($post->ID, $this->meta_key_img,  false )[0];
+		?>
+		
+			<style>
+				.engraving-image {
+					position: relative;
+				}
+				.engraving-text-container {
+					position: absolute;
+					top: 0;
+					width: 100%;
+					height: 100%;
+					z-index: 1;
+					text-align: center;
+				}
+				#engraving-text {
+					display: block;
+					position: relative;
+					top: 50%;
+					transform: translateY(-50%);
+					font-size: 1.2em;
+				}
+				#engraving-container img{
+					width: 100%;
+				}
+				.engraving-options{
+					text-align: center;
+				}
+				span#engraving-text {
+				    color: white;
+				    text-shadow: 1px 1px 1px black;
+				}
+				.engraving-option {
+				    display: flex;
+				    justify-content: space-between;
+				}
+			</style>
+			<div id="engraving-container" class="engraving-container">
+				<div class="engraving-image">
+					<span class="img">
+						<?php if($name){ ?>
+						<img src="<?php echo $name; ?>" alt="produit"/>
+						<?php } ?>
+					</span>
+					<div class="engraving-text-container">
+						<span id="engraving-text">Exemple</span>
+					</div>
+					<input type="hidden" name="<?php echo $this->meta_key_img ?>" id="<?php echo $this->meta_key_img ?>">
+				</div>
+				<div class="engraving-options">
+					<div class="engraving-option">
+						<button class="misha_upload_image_button button button-primary button-large">AJOUTER</button>
+						<button class="misha_remove_image_button button-link delete-attachment" style="color: red;text-transform: lowercase;">SUPPRIMER</button>
+					</div>
+				</div>
+			</div>
+			
+			
+			<script>
+				
+				jQuery(function($){
+				    /*
+				     * Select/Upload image(s) event
+				     */
+				    $('body').on('click', '.misha_upload_image_button', function(e){
+				        e.preventDefault();
+
+				            var button = $(this),
+				                custom_uploader = wp.media({
+				            title: 'Insert image',
+				            library : {
+				                // uncomment the next line if you want to attach image to the current post
+				                // uploadedTo : wp.media.view.settings.post.id, 
+				                type : 'image'
+				            },
+				            button: {
+				                text: 'Use this image' // button label text
+				            },
+				            multiple: false // for multiple image selection set to true
+				        }).on('select', function() { // it also has "open" and "close" events 
+				            var attachment = custom_uploader.state().get('selection').first().toJSON();
+				            $(".engraving-image > span.img").html('<img class="true_pre_image" src="' + attachment.url + '" style="max-width:95%;display:block;" />').next().val(attachment.id).next().show();
+				            $("#<?php echo $this->meta_key_img ?>").val(attachment.url);
+				        })
+				        .open();
+				    });
+
+				    /*
+				     * Remove image event
+				     */
+				    $('body').on('click', '.misha_remove_image_button', function(){
+				        $(this).hide().prev().val('').prev().addClass('button').html('Upload image');
+				        $("#<?php echo $this->meta_key_img ?>").val('');
+				        return false;
+				    });
+
+				});
+				
+			</script>
+		<?php
+		$t=''; 
+		echo $template;
+
+	}
+	function misha_save( $post_id, $post) {
+	    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+	        return $post_id;
+	    
+	    $meta_key = $this->meta_key_img;
+		if($post->post_type=="product"){
+	            if ( ! current_user_can( 'edit_post', $post_id ) ) 
+	                    return $post_id;
+	            $value = esc_textarea( $_POST[$meta_key] );
+	            update_post_meta( $post_id, $meta_key, $value );
+	        }
+	    return $post_id;
+	}
 	 /* 
 	 * Add Custom Content Before 'Add To Cart' Button On Product Page
 	 */
@@ -51,7 +179,6 @@ class SignCustomProduct {
 				.engraving-image {
 					position: relative;
 				}
-				
 				.engraving-text-container {
 					position: absolute;
 					top: 0;
@@ -60,7 +187,6 @@ class SignCustomProduct {
 					z-index: 1;
 					text-align: center;
 				}
-				
 				#engraving-text {
 					display: block;
 					position: relative;
@@ -68,7 +194,6 @@ class SignCustomProduct {
 					transform: translateY(-50%);
 					font-size: 1.2em;
 				}
-			
 			</style>
 
 			<div class="engraving-selector-container">
@@ -95,9 +220,7 @@ class SignCustomProduct {
 					<div class="engraving-option">
 						<label for="customer_engraving_font">Police</label>
 						<select id="customer_engraving_font" name="customer_engraving_font">';
-						
 						$fonts = $this->getFonts();
-						
 						foreach($fonts as $key => $value){
 							$template .= '<option value="'. $key .'">' . $value . '</option>';
 						}
